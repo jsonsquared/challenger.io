902eb6498e3e248983c46cd1c830a326a9be969b
Jason McLeod <pixelparty@gmail.com>
2012-11-10 17:17:17 -0500

    new bullet code, and sprite

--- public/javascripts/bullet.js
+++ public/javascripts/bullet.js
@@ -2,7 +2,7 @@ function Bullet(options) {
     var self = this;
     options = options || {}
 
-    this.speed = 10;
+    this.speed = 16;
     this.delta = 1;
 
     this.trajectoryX = options.endX - options.x;
@@ -13,16 +13,9 @@ function Bullet(options) {
     this.owner = options.owner;
     this.spawnTime = new Date();
 
-    this.sprite = new createjs.Shape();
-    this.sprite.graphics.beginFill('#FFF').drawEllipse(0,0,5,5);
-    this.sprite.x = options.x;
-    this.sprite.y = options.y;
-
-    stage.addChild(this.sprite)
-
     this.spriteSheet = new createjs.SpriteSheet({
-        images: [assets.bullet],
-        frames: {width:8, height:16, regX:4, regY:8},
+        images: [assets.bullet.img],
+        frames: {width:8, height:16, regX:4, regY:16},
         animations: {
             alive:{frames:[0], frequency:5}
         }
@@ -32,7 +25,11 @@ function Bullet(options) {
     this.sprite.scaleX = this.sprite.scaleY = .75
     this.sprite.gotoAndPlay('alive')
 
-    stage.addChild(this.sprite)
+    this.sprite.rotation = players[this.owner].rotation - 90
+    this.sprite.x = options.x;
+    this.sprite.y = options.y;
+
+    stage.addChildAt(this.sprite,2)
 
     $(document).bind('tick', function() {
         if(!self.removed) {
@@ -40,11 +37,9 @@ function Bullet(options) {
             var x = self.delta * self.trajectoryX
             var y = self.delta * self.trajectoryY
 
-
-            if(!blocked(self.sprite.x, self.sprite.y, 1) && !playerHit(self)) {
+            if(!blocked(self.sprite.x, self.sprite.y, 2) && !playerHit(self)) {
                 self.sprite.x += x
                 self.sprite.y += y
-
             } else {
                 console.log('remove bullet sprite')
                 stage.removeChild(self.sprite)
@@ -53,7 +48,7 @@ function Bullet(options) {
                 delete self;
             }
         }
-    })
+    });
 
     this.remove = function() {
         this.onRemove();
