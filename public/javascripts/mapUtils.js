3c2d13ec627b9cd35a7af5182c56be82325ca531
Jason McLeod <pixelparty@gmail.com>
2012-11-10 12:25:57 -0500

    graphics

--- public/javascripts/mapUtils.js
+++ public/javascripts/mapUtils.js
@@ -1,19 +1,31 @@
 function parseMap() {
 
-    for(var y=0;y<map.length;y++) {
-        var row = map[y].split('');
-        for(var x=0;x<row.length;x++) {
+    var mapImage = new Image();
+    mapImage.src = '/assets/images/map.jpg'
+    mapImage.onload = function() {
+        var bitmap = new createjs.Shape()
+        bitmap.graphics.beginBitmapFill(this).drawRect(0, 0, canvas_main.width, canvas_main.height);
+        bitmap.x = 0;
+        bitmap.y = 0;
 
-            var tile = map[y][x];
-            if(tile=='0') {
-                var sprite = new createjs.Shape();
-                sprite.graphics.beginFill('#FFF').rect(0,0,tileSize,tileSize)
-                sprite.x = x * tileSize
-                sprite.y = y * tileSize
-                walls.push({x:x,y:y,sprite: sprite})
-                stage.addChild(sprite)
+        stage.addChild(bitmap)
+
+        for(var y=0;y<map.length;y++) {
+            var row = map[y].split('');
+            for(var x=0;x<row.length;x++) {
+
+                var tile = map[y][x];
+                if(tile=='0') {
+                    var sprite = new createjs.Shape();
+                    sprite.graphics.beginFill('#FFF').rect(0,0,tileSize,tileSize)
+                    sprite.x = x * tileSize
+                    sprite.y = y * tileSize
+                    walls.push({x:x,y:y,sprite: sprite})
+                    stage.addChild(sprite)
+                }
             }
         }
+
     }
 }
 
