3ac5475587bbc11c109671ef1c587877661b4ec9
Jason Barry <jay@jcbarry.com>
2012-11-09 22:42:12 -0500

    merge conflict

--- config/routes.js
+++ config/routes.js
@@ -2,4 +2,8 @@ exports.init = function(app) {
     app.get('/', function(req,res) {
         res.render('index', {title: "challenger.io"});
     });
+
+    var instances = require('../controllers/instances').init(app);
+    app.get('/instance', instances.index);
+    app.get('/instance/:id', instances.show);
 }
