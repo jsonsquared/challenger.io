db25e5d16b9531cd520cb8139397e0f33f204fba
Jason Barry <jay@jcbarry.com>
2012-11-11 01:09:01 -0500

    fix the production config file

--- config/env/production.config.js
+++ config/env/production.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     port: 80,
-    ratchet_key: process.env.ratchet_key
+    ratchet_key: process.env.ratchet_key,
+    instance: {
+        tile_size: 16,
+        player_limit: 16,
+    }
 };
