8795ba71117c787d6ef9eb85dae546d0673caa61
Jason McLeod <pixelparty@gmail.com>
2012-11-11 13:57:48 -0500

    easing lib, messages class

---
+++ public/javascripts/messages.js
@@ -0,0 +1,30 @@
+var messages = [];
+function Message(text, options) {
+    var options = options || {}
+
+    this.font = options.font || 'bold 20px arial';
+    this.color = options.color || '#FFF';
+    this.text = text
+    this.x = options.x || 0
+    this.y = options.y || 400
+
+    this.show = function(options) {
+
+        var $markup = $([
+            '<div class="message">' + this.text + '</div>'
+        ].join(''))
+
+        $('#messages').append($markup)
+        $markup.css({fontSize:0}).animate({fontSize:20, top:-100},{
+            duration: 1000,
+            easing: 'easeOutQuint',
+            complete: function() {
+                $(this).animate({fontSize:0},100, function() { $(this).remove(); })
+            }
+        })
+
+    }
+
+    this.show(options)
+
+}
