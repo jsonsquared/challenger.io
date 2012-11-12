1a24b4225bcedcb3f505c36c78a4e08306c97354
Jason McLeod <pixelparty@gmail.com>
2012-11-12 15:55:00 -0500

    cant ship to nodeknockout instance anymore :/

--- shipit.sh
+++ shipit.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
-# echo "Deploying!"
-./node_modules/.bin/jitsu login --username nko3-challenger-io --password +wxBxrtv2dHBKEA5
-./node_modules/.bin/jitsu deploy --confirm
-
-# Ratchet.io
-ACCESS_TOKEN=18a7fb80ec994e2cbdd54660fabdb8c5
-ENVIRONMENT=production
-LOCAL_USERNAME=`whoami`
-REVISION=`git log -n 1 --pretty=format:"%H"`
-COMMENT=`git log -1 --pretty=%B`
-
-curl https://submit.ratchet.io/api/1/deploy/ \
-  -F access_token=$ACCESS_TOKEN \
-  -F environment=production \
-  -F revision=$REVISION \
-  -F local_username=$LOCAL_USERNAME \
-  -F "comment=$COMMENT"
+# #!/bin/bash
+# # echo "Deploying!"
+# ./node_modules/.bin/jitsu login --username nko3-challenger-io --password +wxBxrtv2dHBKEA5
+# ./node_modules/.bin/jitsu deploy --confirm
+#
+# # Ratchet.io
+# ACCESS_TOKEN=18a7fb80ec994e2cbdd54660fabdb8c5
+# ENVIRONMENT=production
+# LOCAL_USERNAME=`whoami`
+# REVISION=`git log -n 1 --pretty=format:"%H"`
+# COMMENT=`git log -1 --pretty=%B`
+#
+# curl https://submit.ratchet.io/api/1/deploy/ \
+#   -F access_token=$ACCESS_TOKEN \
+#   -F environment=production \
+#   -F revision=$REVISION \
+#   -F local_username=$LOCAL_USERNAME \
+#   -F "comment=$COMMENT"
