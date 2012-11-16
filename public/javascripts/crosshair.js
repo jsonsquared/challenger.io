c8ce064404464ca322808d5fccd40101c6922569
Jason McLeod <pixelparty@gmail.com>
2012-11-16 14:17:37 -0500

    dont need a full blown sprite sheet for this;

--- public/javascripts/crosshair.js
+++ public/javascripts/crosshair.js
@@ -1,17 +1,6 @@
 function Crosshair() {
-    this.spriteSheet = new createjs.SpriteSheet({
-        images: [assets.crosshair.img],
-        frames: {width:32, height:32, regX:8, regY:8},
-        animations: {
-            main:{frames:[0], frequency:5}
-        }
-    });
-
-    this.sprite = new createjs.BitmapAnimation(this.spriteSheet);
+    this.sprite = new createjs.Bitmap(assets.crosshair.img);
     this.sprite.x = canvas_main.width/2 - TILE_SIZE
     this.sprite.y = canvas_main.height/2 - TILE_SIZE
-
-    this.sprite.gotoAndPlay('main')
-
     stage_over.addChild(this.sprite)
 }
