d620635e2999e72c3e0e56c20d843e5e572cc485
Jason McLeod <pixelparty@gmail.com>
2012-11-16 19:33:21 -0500

    feeling ok about lighting... lightened up map.jpg

--- public/javascripts/render.js
+++ public/javascripts/render.js
@@ -1,16 +1,15 @@
 var CLEAR = 1;
-var FILL = .4;
+var FILL = .5;
 var lightLayer;         // easeljs stage for lights. shared between initLights, processRaycasting
 var darkmask;           // illumination.js canvas. shared between initLights, processRaycasting
 var raycaster = {}      // illumination.js lightsource, position is updated through
 var lightLayers = 0;
-var light1 = false;
+var lighting;
 function render() {
     if(!connected || !me) return
     stage_under.update();
     stage_over.update();
     processRaycasting();
-    lightingEngine.render()
     lightLayer.draw(canvas_main_ctx)
     crosshairLayer.draw(canvas_main_ctx)
 }
@@ -19,8 +18,8 @@ function initLights() {
     var objects = []
     raycaster = new illuminated.Lamp({
         position: new illuminated.Vec2(100, 250),
-        distance: 350,
-        radius: 0,
+        distance: 250,
+        radius: 125,
         samples: 1,
         angle:0
     });
@@ -32,12 +31,12 @@ function initLights() {
         });
     }
 
-    lighting1 = new illuminated.Lighting({
+    lighting = new illuminated.Lighting({
         light: raycaster,
         objects: objects
     });
 
-    darkmask = new illuminated.DarkMask({ lights: [raycaster] });
+    darkmask = new illuminated.DarkMask({ lights: [raycaster], color: 'rgba(0,0,0,.5)' });
 
     lightLayer = new createjs.Bitmap(canvas_lighting)
     stage_under.addChildAt(lightLayer,3)
@@ -46,25 +45,14 @@ function initLights() {
 }
 function processRaycasting () {
 
-    var touching = lighting1.compute(canvas_lighting.width, canvas_lighting.height);
-    darkmask.compute(canvas_lighting.width, canvas_lighting.height);
-
-    canvas_lighting_ctx.globalCompositeOperation = "source-out";
-    canvas_lighting_ctx.fillStyle = "rgba(0,0,0,1)";
-    canvas_lighting_ctx.fillRect(0, 0, canvas_lighting.width, canvas_lighting.height);
-
     canvas_lighting_ctx.globalCompositeOperation = "destination-out";
-    lighting1.render(canvas_lighting_ctx);
+    canvas_lighting_ctx.fillRect(0, 0, canvas_lighting.width, canvas_lighting.height);
+    lighting.compute(canvas_lighting.width, canvas_lighting.height);
+    lighting.render(canvas_lighting_ctx);
 
-    canvas_lighting_ctx.fillStyle = "rgba(0,0,0," + FILL + ")";
-
-    if(CLEAR) {
-        for(var o = 0; o< touching.length; o++) {
-           canvas_lighting_ctx.clearRect(touching[o].points[0].x, touching[o].points[0].y, TILE_SIZE, TILE_SIZE)
-           canvas_lighting_ctx.fillRect(touching[o].points[0].x, touching[o].points[0].y, TILE_SIZE, TILE_SIZE)
-       }
-    }
-
-    canvas_lighting_ctx.globalCompositeOperation = "destination-over";
+    canvas_lighting_ctx.globalCompositeOperation = "source-over"
+    canvas_lighting_ctx.fillStyle = "rgba(0,0,0,.15)";
+    darkmask.compute(canvas_lighting.width, canvas_lighting.height);
     darkmask.render(canvas_lighting_ctx);
 }
+var A = true
