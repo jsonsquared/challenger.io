e00014d23b8b3074231b43e7a880d86b50f71c39
Jason McLeod <pixelparty@gmail.com>
2012-11-18 13:34:42 -0500

    streetlights, removed google font

--- public/javascripts/render.js
+++ public/javascripts/render.js
@@ -4,7 +4,9 @@ var lightLayer;         // easeljs stage for lights. shared between initLights, 
 var darkmask;           // illumination.js canvas. shared between initLights, processRaycasting
 var raycaster = {}      // illumination.js lightsource, position is updated through
 var lightLayers = 0;
-var lighting;
+var lights= [];
+var myLighting
+var lighting= [];
 function render() {
     if(!connected || !me) return
     stage_under.update();
@@ -31,27 +33,59 @@ function initLights() {
         });
     }
 
-    lighting = new illuminated.Lighting({
+    myLighting = new illuminated.Lighting({
         light: raycaster,
         objects: objects
     });
 
-    darkmask = new illuminated.DarkMask({ lights: [raycaster], color: 'rgba(0,0,0,.9)' });
+
+    // street lights
+    lights.push(new illuminated.Lamp({position: new illuminated.Vec2(672, 115),distance: 150,radius: 75,samples: 1,angle:0}));
+    lighting.push(new illuminated.Lighting({light: lights[lights.length-1],objects:objects}))
+
+    lights.push(new illuminated.Lamp({position: new illuminated.Vec2(353, 305),distance: 150,radius: 75,samples: 1,angle:0}));
+    lighting.push(new illuminated.Lighting({light: lights[lights.length-1],objects:objects}))
+
+    lights.push(new illuminated.Lamp({position: new illuminated.Vec2(149, 496),distance: 150,radius: 75,samples: 1,angle:0}));
+    lighting.push(new illuminated.Lighting({light: lights[lights.length-1],objects:objects}))
+
+    lights.push(new illuminated.Lamp({position: new illuminated.Vec2(609, 415),distance: 150,radius: 75,samples: 1,angle:0}));
+    lighting.push(new illuminated.Lighting({light: lights[lights.length-1],objects:objects}))
+
+    for(l = 0;l<lighting.length;l++) {
+        lighting[l].compute(canvas_lighting.width, canvas_lighting.height);
+    }
+
+    lights.push(raycaster)
+
+    darkmask = new illuminated.DarkMask({
+        lights: lights,
+        color: 'rgba(0,0,0,.9)'
+    });
 
     lightLayer = new createjs.Bitmap(canvas_lighting)
     stage_under.addChildAt(lightLayer,3)
     crosshairLayer = new createjs.Bitmap(canvas_crosshair)
     stage_under.addChildAt(crosshairLayer,4)
+
 }
+
 function processRaycasting () {
+
+    canvas_lighting_ctx.fillStyle = "rgba(0,0,0,.2)";
+    canvas_lighting_ctx.fillRect(0, 0, canvas_lighting.width, canvas_lighting.height);
 
     canvas_lighting_ctx.globalCompositeOperation = "destination-out";
     canvas_lighting_ctx.fillRect(0, 0, canvas_lighting.width, canvas_lighting.height);
-    lighting.compute(canvas_lighting.width, canvas_lighting.height);
-    lighting.render(canvas_lighting_ctx);
+    myLighting.compute(canvas_lighting.width, canvas_lighting.height);
+    myLighting.render(canvas_lighting_ctx);
+
+    for(var l = 0; l < lighting.length; l++) {
+        lighting[l].render(canvas_lighting_ctx);
+    }
 
     canvas_lighting_ctx.globalCompositeOperation = "source-over"
-    canvas_lighting_ctx.fillStyle = "rgba(0,0,0,.15)";
+
     darkmask.compute(canvas_lighting.width, canvas_lighting.height);
     darkmask.render(canvas_lighting_ctx);
 }
