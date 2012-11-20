418131573f80d584bd339d9244a76ee585dd7abd
Jason McLeod <pixelparty@gmail.com>
2012-11-20 17:58:24 -0500

    Bomb item, player takes damage on pickup

--- public/assets/itemsDB.js
+++ public/assets/itemsDB.js
@@ -26,33 +26,44 @@ module.exports =  [
             p.debuff()
         }
     },
-// {
-//   name: 'Small Health Pack',
-//   color:'orange',
-//   buff:function(p) {
-//     p.health = Math.min(p.health + p.health * 0.15, 100)
-//     p.refresh()
-//   },
-//   debuff:function(){}
-// },
-// {
-//   name: 'Large Health Pack',
-//   color:'red',
-//   buff:function(p) {
-//     p.health = Math.min(p.health + p.health * 0.30, 100)
-//     p.refresh()
-//   },
-//   debuff:function(){}
-// },
+    {
+        name: 'Small Health Pack',
+        color:'orange',
+        buff:function(p) {
+            p.health = Math.min(p.health+25,100)
+            p.buff()
+        },
+        debuff:function(){}
+    },
+    {
+        name: 'Large Health Pack',
+        color:'purple',
+        buff:function(p) {
+            p.health = Math.min(p.health+50,100)
+            p.debuff()
+        },
+        debuff:function(){}
+    },
     {
         name: 'Sprint',
         color:'white',
         buff:function(p) {
-            p.moveDistance = MOVE_DISTANCE * 2;//1.15
+            p.moveDistance = MOVE_DISTANCE * 1.15
             p.buff()
         },
         debuff:function(p){
             p.moveDistance = MOVE_DISTANCE
+            p.debuff()
+        }
+    },
+    {
+        name: 'Bomb',
+        color:'red',
+        buff:function(p) {
+            p.hurtByItem(this, 10)
+            p.buff()
+        },
+        debuff:function(p){
             p.debuff()
         }
     }
