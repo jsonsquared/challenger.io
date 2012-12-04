ca4b58e72590029587386f04363159190992c8cd
Jason McLeod <pixelparty@gmail.com>
2012-12-04 16:17:01 -0500

    cap stuff

---
+++ config/deploy.rb
@@ -0,0 +1,41 @@
+require 'capistrano/ext/multistage'
+load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+set :stages, %w(prod)
+set :default_stage, "prod"
+
+set :application, "pewpew"
+set :node_file, "server.js"
+
+set :repository, "git@github.com:jsonsquared/challenger.io.git"
+set :branch, "threejs"
+set :deploy_to, "/var/www/#{application}"
+
+set :keep_releases, 5
+set :scm, :git
+set :deploy_via, :remote_cache
+set :normalize_asset_timestamps, false
+# default_run_options[:shell] = 'bash'
+
+namespace :deploy do
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "service #{application} start"
+  end
+
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "service #{application} stop"
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "service #{application} restart || service #{application} start"
+  end
+
+  task :install_dependent_packages, :roles => :app do
+    run "cd #{release_path} && npm install --production --quiet"
+  end
+
+  task :migrate do
+  end
+end
+
+after 'deploy:update_code', 'deploy:install_dependent_packages'
+after 'deploy', 'deploy:cleanup'
