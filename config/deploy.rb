fa85ebb4251e107e559a1deba2a44c21ab30b41b
Jason McLeod <pixelparty@gmail.com>
2012-12-06 14:48:00 -0500

    192381982323rd time is the charm

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ namespace :deploy do
 
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "chmod +x /www/#{application}/current/stop.sh && cd /www/#{application}/current/ && ./stop.sh || false"
-    run "chmod +x /www/#{application}/current/start.sh && cd /www/#{aaplication}/current/ && ./start.sh || false"
+    run "chmod +x /www/#{application}/current/start.sh && cd /www/#{application}/current/ && ./start.sh || false"
   end
 
   # task :init_forever, :roles => :app, :except => { :no_release => true } do
